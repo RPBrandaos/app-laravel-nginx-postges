6509a2aeddbdf74c894fe97d1d974865b7f94738
Ronald <ronald.brandao@portovelho.ro.gov.br>
2023-04-11 22:42:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Laravel Docker Nginx build using PHP-FPM and OP_CACHE.
+
+Step 1: Run `docker-compose up --build --force-recreate -d`
+
+Default PHP port is 8000. Connect via `http://127.0.0.1:8000` or `http://localhost:8000`
---
+++ docker/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo "env file exists."
+fi
+
+php artisan key:generate
+php artisan config:clear
+php artisan view:clear
+php artisan cache:clear
+php artisan migrate
+php artisan db:seed
+
+php-fpm -D
+nginx -g "daemon off;"
